c1942263d93beec0d653039822e258c0ef86defd
AminRafaey <aminrafaey543@gmail.com>
2021-04-24 12:38:26 +0500

    make infinite scrolling remove from chat

--- src/components/Inbox/ChatArea/index.js
+++ src/components/Inbox/ChatArea/index.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import InfiniteScroll from 'react-infinite-scroll-component';
 import Reciever from './Reciever';
 import Sender from './Sender';
 import TypingArea from './TypingArea';
@@ -10,7 +9,7 @@ import {
   addMessages,
 } from '../../../Context/Chat';
 import { useSocketState } from '../../../Context/Socket';
-import { styled, Box, CircularProgress } from '@material-ui/core';
+import { styled, Box } from '@material-ui/core';
 
 const ChatAreaWrapper = styled(Box)({
   display: 'flex',
@@ -24,18 +23,12 @@ const TypingAreaWrapper = styled(Box)({
   height: '30vh',
   background: '#ffff',
 });
+
 const ChatsWrapper = styled(Box)({
   overflow: 'auto',
   display: 'flex',
   flexDirection: 'column-reverse',
   padding: '0px 5px 0px 5px',
-  height: '70vh',
-});
-
-const LoadingWrapper = styled(Box)({
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
   height: '70vh',
 });
 
@@ -49,53 +42,37 @@ function ChatArea(props) {
   const [selectedMedia, setSelectedMedia] = useState({});
 
   useEffect(() => {
-    socket.on('get-contact-messages', (res) => {
-      addMessages(chatDispatch, {
-        jid: res.jid,
-        messages: res.messages.messages,
+    if (chatState.find((c) => c.jid === currentChatJid).messages.length < 1) {
+      socket.on('get-contact-messages', (res) => {
+        addMessages(chatDispatch, {
+          jid: res.jid,
+          messages: res.messages.messages,
+        });
       });
-    });
-    socket.emit('get-contact-messages', currentChatJid);
-  }, []);
+
+      socket.emit('get-contact-messages', currentChatJid);
+    }
+  }, [currentChatJid]);
 
   useEffect(() => {
     Object.entries(selectedTemplate).length > 0 &&
       setMessage(selectedTemplate.content);
   }, [selectedTemplate]);
 
-  const fetchMoreData = () => {
-    socket.emit('get-contact-messages', currentChatJid);
-  };
   return (
     <ChatAreaWrapper>
       <ChatsWrapper id="scrollableDiv" className="Chat-Box-Styled-Scroll">
-        <InfiniteScroll
-          dataLength={
-            chatState.find((c) => c.jid === currentChatJid)['messages'].length
-          }
-          next={fetchMoreData}
-          hasMore={true}
-          loader={
-            <LoadingWrapper>
-              <CircularProgress />
-            </LoadingWrapper>
-          }
-          style={{ display: 'flex', flexDirection: 'column-reverse' }}
-          inverse={true}
-          scrollableTarget="scrollableDiv"
-        >
-          {chatState
-            .find((c) => c.jid === currentChatJid)
-            ['messages'].map((m, index) => {
-              if (m.message && m.message.conversation) {
-                return m.key.fromMe ? (
-                  <Sender message={m} key={index} />
-                ) : (
-                  <Reciever message={m} key={index} />
-                );
-              }
-            })}
-        </InfiniteScroll>
+        {chatState
+          .find((c) => c.jid === currentChatJid)
+          ['messages'].map((m, index) => {
+            if (m.message && m.message.conversation) {
+              return m.key.fromMe ? (
+                <Sender message={m} key={index} />
+              ) : (
+                <Reciever message={m} key={index} />
+              );
+            }
+          })}
       </ChatsWrapper>
 
       <TypingAreaWrapper>
